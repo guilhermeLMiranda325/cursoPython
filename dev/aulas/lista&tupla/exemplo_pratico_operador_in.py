9a703498b8036de4546bcc03fe202a914bd187aa
Guilherme Miranda <guilherme.miranda325@gmail.com>
2019-09-16 09:36:18 -0300

    Aulas de Lista e Tupla.

---
+++ dev/aulas/lista&tupla/exemplo_pratico_operador_in.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+# a = 10
+# b = 25
+# c = 66
+#
+# x = int(input("Digite um número: "))
+#
+# # if (x == a or x == b or x == c):
+# # if (x in [10, 25, 66]):
+# if (x in [a, b, c]):
+#     print("Está contido")
+# else:
+#     print("NÃO está contido")
+
+#----------------------------------------
+
+cores = ["azul", "amarelo", "vermelho", "branco"]
+
+while True:
+    cor = input("Digite o nome de uma cor ou então, "
+                "0 para sair do programa: ")
+    if cor == "0":
+        break
+    elif cor in cores:
+        print("Essa cor está contida.")
+    else:
+        print("Essa cor NÃO está contida.")
