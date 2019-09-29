edbd943a85a939627dbcf0a4fa3df0761d068ec6
Guilherme Lourenço de Miranda <guilherme.miranda325@gmail.com>
2019-09-29 13:15:44 -0300

    Lista E Tupla

---
+++ dev/aulas/lista&tupla/exemplo_pratico_operador_in.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# a = 10
+# b = 25
+# c = 66
+#
+# x = int(input("Digite um número: "))
+#
+# # if(x == a or x == b or x == c):
+# # if (x in [10, 25, 66]):
+# if (x in [a, b, c]):
+#     print("Está contido.")
+# else:
+#     print("NÃO está contido.")
+
+#---------------------------------------
+
